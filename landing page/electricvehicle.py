a0ffbef7f8281e866daa5ad0442c5ff2bc7aeb9a
Fabiola Cartagena <fabiola.cartagena.14@gmail.com>
2020-08-15 13:45:03 -0700

    Organize folder

--- landing page/electricvehicle.py
+++ landing page/electricvehicle.py
@@ -1,9 +1,9 @@
 from flask import Flask, redirect, url_for, render_template
 
-app= Flask(__name__)
+app= Flask(__name__, static_url_path='/static')
 
 @app.route("/")
-def index():
+def home():
     return render_template("index.html")
 
 
